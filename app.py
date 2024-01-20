f5fcb2afe997dea9351cf22d2bfc4e2be4c50329
yusuf <yusufbello@live.com>
2024-01-20 21:02:55 +0100

    app.py file added

---
+++ app.py
@@ -0,0 +1,15 @@
+# age = 31
+# name = input('enter name')
+# if age <= 30:
+#     print(f'{name} can apply')
+
+
+# #Using loop
+# for x in range(5,51,5):
+#     print (x)
+
+#while loop
+i = 5
+while i <= 50:
+    print (i)
+    i = i +5
